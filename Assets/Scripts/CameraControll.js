198a12bc5bba358221b2b6dc60e037ed4e7ecbd1
Zachary Peterson <zachary.troy.peterson@gmail.com>
2016-11-05 13:07:07 -0400

    Adding Menu and Level Files
    Added the files that Hui and Ivy created for the draft of the menu,
    credits, and game level

---
+++ Assets/Scripts/CameraControll.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+public var currentMount:Transform;
+public var speedFactor:float = 0.1;
+//public var zoomFactor = 1.0;
+//public var cameraComp:Camera;
+
+//private var lastPosition:Vector3;
+
+function Start () {
+   //lastPosition = transform.position;
+}
+
+function Update () {
+   transform.position = Vector3.Lerp(transform.position,currentMount.position,speedFactor);
+   transform.rotation = Quaternion.Slerp(transform.rotation,currentMount.rotation,speedFactor);
+
+   //var velocity = Vector3.Magnitude(transform.position-lastPosition);
+   //cameraComp.fieldOfView = 60+velocity*zoomFactor;
+
+   //lastPosition = transform.position;
+}
+
+function setMount(newMount:Transform){
+   currentMount = newMount;
+   Debug.Log ("You have clicked the button!");
+
+}
