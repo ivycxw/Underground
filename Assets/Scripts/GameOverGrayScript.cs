2ddb53635481a091753b277f1af3b080c2070060
Sangmin Lee <ibombing@gmail.com>
2016-11-08 23:45:40 -0500

    Grayscale effect is added. It will be used when our character dies. Currently, it will be triggered by 'shitft'

---
+++ Assets/Scripts/GameOverGrayScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+
+public class GameOverGrayScript : MonoBehaviour {
+	private bool isGray;
+	private Grayscale effect;
+
+	void Start () {
+		isGray = false;
+		effect = GetComponent<Grayscale> ();
+	}
+
+	void Update () {
+		// will be triggered by Player's Health
+		if(Input.GetButtonDown("Fire3"))
+		{
+			isGray = !isGray;
+			effect.enabled = isGray;
+		}
+	}
+}
