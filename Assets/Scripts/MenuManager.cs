16c096a4e646f2a0a35ead8bba58c1d709fef9b8
Zachary Peterson <zachary.troy.peterson@gmail.com>
2016-11-11 14:32:16 -0500

    Controller Navigation
    Added controller and keyboard based menu navigation. Hooked up play to
    basic level. Added placeholder text to level to show that we have not
    done gameplay yet.

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -14,11 +17,31 @@ public class MenuManager : MonoBehaviour
 	public RectTransform ScrollingCreditText;
 	public float CreditScrollRate;
 
+	public Button MainMenuPlayButton;
+	public Button MainMenuMusicButton;
+	public Button MainMenuCreditsButton;
+	public Button PlayMenuRoom1Button;
+	public Button PlayMenuRoom2Button;
+	public Button PlayMenuRoom3Button;
+	public Button PlayMenuRoom4Button;
+	public Button PlayMenuBackButton;
+	public Button CreditsMenuBackButton;
+
 	private Vector3 mCameraOffset;
 	private float mCurrentRotation;
 
 	private bool mCurrentlyScrolling;
 	private float mStartingCreditsHeight;
+
+	private enum EMenuState
+	{
+		MainMenu,
+		PlayMenu,
+		CreditsMenu
+	};
+
+	private EMenuState mMenuState;
+
 
 	// Use this for initialization
 	void Start ()
@@ -27,6 +50,15 @@ public class MenuManager : MonoBehaviour
 		mCameraOffset = Camera.transform.position - CameraTarget.position;
 		Camera.transform.rotation = Quaternion.LookRotation(-mCameraOffset);
 		mCurrentRotation = 0.0f;
+
+		mMenuState = EMenuState.MainMenu;
+
+		MainMenu.GetComponent<Canvas>().enabled = true;
+		RoomMenu.GetComponent<Canvas>().enabled = false;
+		CreditsMenu.GetComponent<Canvas>().enabled = false;
+
+		MainMenuPlayButton.Select();
+
 	}
 
 	// Update is called once per frame
@@ -45,6 +77,24 @@ public class MenuManager : MonoBehaviour
 		Vector3 newOffset = Quaternion.AngleAxis(mCurrentRotation, Vector3.up) * mCameraOffset;
 		Camera.transform.position = CameraTarget.position + newOffset;
 		Camera.transform.rotation = Quaternion.LookRotation(-newOffset);
+
+		switch (mMenuState)
+		{
+			case EMenuState.MainMenu:
+				break;
+			case EMenuState.PlayMenu:
+				if (ControlInputWrapper.GetButtonDown(ControlInputWrapper.Buttons.B))
+				{
+					FromRoomToMenu();
+				}
+				break;
+			case EMenuState.CreditsMenu:
+				if (ControlInputWrapper.GetButtonDown(ControlInputWrapper.Buttons.B))
+				{
+					FromCreditsToMenu();
+				}
+				break;
+		};
 	}
 
 	public void StartScrollingCredits()
@@ -60,27 +110,44 @@ public class MenuManager : MonoBehaviour
 
 	public void FromMenuToCredits()
 	{
-		MainMenu.SetActive(false);
-		CreditsMenu.SetActive(true);
+		EventSystem.current.SetSelectedGameObject(null);
+		MainMenu.GetComponent<Canvas>().enabled = false;
+		CreditsMenuBackButton.Select();
+		CreditsMenu.GetComponent<Canvas>().enabled = true;
 		StartScrollingCredits();
+		mMenuState = EMenuState.CreditsMenu;
 	}
 
 	public void FromCreditsToMenu()
 	{
-		MainMenu.SetActive(true);
-		CreditsMenu.SetActive(false);
+		EventSystem.current.SetSelectedGameObject(null);
+		CreditsMenu.GetComponent<Canvas>().enabled = false;
+		MainMenuPlayButton.Select();
+		MainMenu.GetComponent<Canvas>().enabled = true;
 		StopScrollingCredits();
+		mMenuState = EMenuState.MainMenu;
 	}
 
 	public void FromMenuToRoom()
 	{
-		MainMenu.SetActive(false);
-		RoomMenu.SetActive(true);
+		EventSystem.current.SetSelectedGameObject(null);
+		mMenuState = EMenuState.PlayMenu;
+		MainMenu.GetComponent<Canvas>().enabled = false;
+		PlayMenuRoom1Button.Select();
+		RoomMenu.GetComponent<Canvas>().enabled = true;
 	}
 
 	public void FromRoomToMenu()
 	{
-		MainMenu.SetActive(true);
-		RoomMenu.SetActive(false);
+		EventSystem.current.SetSelectedGameObject(null);
+		RoomMenu.GetComponent<Canvas>().enabled = false;
+		MainMenuPlayButton.Select();
+		MainMenu.GetComponent<Canvas>().enabled = true;
+		mMenuState = EMenuState.MainMenu;
+	}
+
+	public void StartGame()
+	{
+		SceneManager.LoadScene("Scenes/Game Level");
 	}
 }
