8e676f05eb03c549ea3517895b74e30547f52b3e
hfeng49 <hfeng49@gatech.edu>
2016-11-14 09:41:33 -0500

    add Game Level

---
+++ Assets/Resources/Square Fantasy/Scripts/LoadSceneDemo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSceneDemo : MonoBehaviour {
+
+	// Update is called once per frame
+	public void LoadScene (int levelNumber)
+	{
+		Application.LoadLevel(levelNumber);
+	}
+}
