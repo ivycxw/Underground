4b3791d9abdaab6f68a71bc73acbb4f58426c06a
Sangmin Lee <ibombing@gmail.com>
2016-11-14 16:21:33 -0800

    Rotation is added for Coins for Tunnel

---
+++ Assets/Scripts/CoinRotator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinRotator : MonoBehaviour {
+
+	private MeshRenderer mr;
+
+	// Use this for initialization
+	void Start () {
+		mr = GetComponent<MeshRenderer> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mr.transform.Rotate(Vector3.right * Time.deltaTime * 50);
+//		transform.Rotate(Vector3.right * Time.deltaTime * 50);
+	}
+}
