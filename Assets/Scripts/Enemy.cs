76a3ea7fe1f41359a60922163d30eb3f24562e25
Hyun Seo Chung <hchung48@gatech.edu>
2016-11-15 15:36:26 -0500

    Added patrol routes for enemies, added AIs and a behavior tree for all the enemies to follow designated patrol routes, adjusted map layout/settings, and updated navmesh

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof (ThirdPersonCharacter))]
+[RequireComponent(typeof (Animator))]
+public class Enemy : MonoBehaviour {
+
+	private ThirdPersonCharacter character;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		character = GetComponent <ThirdPersonCharacter> ();
+		anim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 velocity = new Vector3 (anim.GetFloat ("Turn") / 360f, 0, anim.GetFloat ("Forward"));
+		character.Move (velocity, false, false);
+	}
+}
