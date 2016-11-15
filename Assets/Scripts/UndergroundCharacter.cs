726117b38a9e843fb6852ed8f5016037b3f515b6
Zachary Peterson <zachary.troy.peterson@gmail.com>
2016-11-15 17:39:55 -0500

    Character Consolidation
    Consolidated relevant scripts for the character into a single
    UndergroundCharacter.cs file including the controller, camera follower,
    game over effect and weapon. Also added health, damaging, and some basic
    death logic.

---
+++ Assets/Scripts/UndergroundCharacter.cs
@@ -0,0 +1,160 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityStandardAssets.ImageEffects;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+[RequireComponent(typeof(Animator))]
+public class UndergroundCharacter : MonoBehaviour
+{
+	public float MaxHealth = 100.0f;
+	public float CameraSmoothingFactor = 5f;
+	public float RespawnTime = 2.0f;
+
+	private ThirdPersonCharacter m_Character;
+	private Transform m_CamTransform;
+	private Vector3 m_CamForward;
+	private Vector3 m_Move;
+	private bool m_Jump;
+	private Animator m_Animator;
+	private List<GameObject> m_PotentialHitObjects;
+	private Vector3 m_CamOffset;
+	private float m_Health;
+	private bool m_Dead;
+	private Grayscale m_DeadEffect;
+
+        
+	void Start()
+	{
+		// Get the camera transform for camera relative controls
+		if (Camera.main != null)
+		{
+			m_CamTransform = Camera.main.transform;
+			m_CamOffset = m_CamTransform.position - transform.position;
+			m_DeadEffect = Camera.main.GetComponent<Grayscale>();
+			if (m_DeadEffect != null)
+			{
+				m_DeadEffect.enabled = false;
+			}
+		}
+		else
+		{
+			Debug.LogWarning("Warning: No main camera found. Underground character needs a Camera tagged \"MainCamera\" for camera-relative controls.");
+		}
+
+		// Get the ThirdPersonCharacter and Animator controls need for the character
+		m_Character = GetComponent<ThirdPersonCharacter>();
+		m_Animator = GetComponent<Animator>();
+
+		// Set up the list of objects that should potentially be hit
+		m_PotentialHitObjects = new List<GameObject>();
+
+		// Set up the current health based on the max health
+		m_Health = MaxHealth;
+		m_Dead = false;
+	}
+
+	void Update()
+	{
+		// Check input for jumping
+		if (!m_Jump)
+		{
+			m_Jump = ControlInputWrapper.GetButtonDown(ControlInputWrapper.Buttons.Y) || Input.GetButtonDown("Jump");
+		}
+
+		// Check to see if the user has requested to attack and set it in the animator
+		m_Animator.SetBool("Attacking", ControlInputWrapper.GetButton(ControlInputWrapper.Buttons.X) || Input.GetButton("Attacking"));
+
+		// If we are in the "hit" part of the Hit animation, check to see if we are colliding with anything
+		if (m_Animator.GetFloat("Hit") > 0.0f)
+		{
+			foreach (GameObject go in m_PotentialHitObjects)
+			{
+				Debug.Log("Hit game object: " + go);
+				// TODO: Damage any NPCs that are hit
+			}
+			// We've hit objects now, so clear the potentials list so that objects don't get hit multiple times in a single attack
+			m_PotentialHitObjects.Clear();
+		}
+	}
+
+	void FixedUpdate()
+	{
+		// Grab the movement axes and our movement
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		// Taken from the third person user controller, calculates relative camera movement
+		if (m_CamTransform != null)
+		{
+			m_CamForward = Vector3.Scale(m_CamTransform.forward, new Vector3(1, 0, 1)).normalized;
+			m_Move = v * m_CamForward + h * m_CamTransform.right;
+		}
+		else
+		{
+			m_Move = v * Vector3.forward + h * Vector3.right;
+		}
+
+		m_Character.Move(m_Move, false, m_Jump);
+		m_Jump = false;
+
+		// Update our camera tracking
+		Vector3 targetCamPos = transform.position + m_CamOffset;
+        m_CamTransform.position = Vector3.Lerp(m_CamTransform.position, targetCamPos, CameraSmoothingFactor * Time.deltaTime);
+		m_CamTransform.rotation = Quaternion.LookRotation(transform.position - m_CamTransform.position);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// Add the hit object to the list of potential hit objects
+		if (!m_PotentialHitObjects.Contains(other.gameObject))
+		{
+			m_PotentialHitObjects.Add(other.gameObject);
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		// Remove the hit object from the list of potential hit objects
+		if (m_PotentialHitObjects.Contains(other.gameObject))
+		{
+			m_PotentialHitObjects.Remove(other.gameObject);
+		}
+	}
+
+	// Called to damage the character
+	public void TakeDamage(float DamageAmount)
+	{
+		if (!m_Dead)
+		{
+			m_Health -= DamageAmount;
+			if (m_Health <= 0.0f)
+			{
+				Died();
+			}
+		}
+	}
+
+	// Called when the character dies
+	private void Died()
+	{
+		m_Dead = true;
+		if (m_DeadEffect != null)
+		{
+			m_DeadEffect.enabled = true;
+		}
+		Invoke("Respawn", RespawnTime);
+	}
+
+	// Called after a timer to respawn the character at the last checkpoint
+	private void Respawn()
+	{
+		m_Dead = false;
+		m_Health = MaxHealth;
+		if (m_DeadEffect != null)
+		{
+			m_DeadEffect.enabled = false;
+		}
+	}
+}
