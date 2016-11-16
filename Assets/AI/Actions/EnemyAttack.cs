fd69dbfe083bba87a39678fea49d30efa880f07d
Hyun Seo Chung <hchung48@gatech.edu>
2016-11-15 22:08:13 -0500

    Added attack sequence for the enemies in the first room with attack animations

---
+++ Assets/AI/Actions/EnemyAttack.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RAIN.Action;
+using RAIN.Core;
+
+
+[RAINAction]
+public class EnemyAttack : RAINAction
+{
+	private UndergroundCharacter player;
+	private Animator anim;
+	
+    public override void Start(RAIN.Core.AI ai)
+    {
+		player = ai.WorkingMemory.GetItem<GameObject> ("player").GetComponent<UndergroundCharacter> ();
+		anim = ai.Body.GetComponent<Animator> ();
+
+        base.Start(ai);
+    }
+
+    public override ActionResult Execute(RAIN.Core.AI ai)
+    {
+		if (Vector3.Distance (ai.Kinematic.Position, player.transform.position) <= 1.3f) {
+			anim.SetTrigger ("Attack");
+			player.TakeDamage (10);
+
+			return ActionResult.SUCCESS;
+		}
+
+		return ActionResult.FAILURE;
+    }
+
+    public override void Stop(RAIN.Core.AI ai)
+    {
+        base.Stop(ai);
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,4 +1,14 @@
-﻿using UnityEngine;
+﻿/*
+    Team Underground
+
+    Xiaowei Chen
+    Hyun Seo Chung
+    Hui Feng
+    Sangmin Lee
+    Zachary Peterson
+*/
+
+using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.Characters.ThirdPerson;
 
@@ -8,6 +18,8 @@ public class Enemy : MonoBehaviour {
 
 	private ThirdPersonCharacter character;
 	private Animator anim;
+
+	private int health = 100;
 
 	// Use this for initialization
 	void Start () {
@@ -21,5 +33,17 @@ public class Enemy : MonoBehaviour {
 	void Update () {
 		Vector3 velocity = new Vector3 (anim.GetFloat ("Turn") / 360f, 0, anim.GetFloat ("Forward"));
 		character.Move (velocity, false, false);
+	}
+
+	void TakeDamage (int damageAmount) {
+		health -= damageAmount;
+
+		if (health <= 0) {
+			Die ();
+		}
+	}
+
+	void Die () {
+		// TODO
 	}
 }
