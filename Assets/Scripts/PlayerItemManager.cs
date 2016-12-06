3fb7ad02556689dd428e2d3cde7530592780ce64
Sangmin Lee <ibombing@gmail.com>
2016-12-06 17:30:57 -0500

    Coin sounds added

---
+++ Assets/Scripts/PlayerItemManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerItemManager : MonoBehaviour {
+	public static bool hasKey;
+	public static long number_of_coins = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
