ea217febc9532a45c5e33d64b1313795b2d42903
Sangmin Lee <ibombing@gmail.com>
2016-12-06 15:58:34 -0500

    AudioListenr and several scripts added

---
+++ Assets/Scripts/BigHPRotator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BigHPRotator : MonoBehaviour {
+
+	private MeshRenderer mr;
+	private MeshCollider mc;
+	private AudioSource source;
+	private float HP_AMOUNT = 50.0f;
+	public AudioClip collectedSound;
+
+	// Use this for initialization
+	void Start () {
+		mr = GetComponent<MeshRenderer> ();
+		mc = GetComponent<MeshCollider> ();
+		source = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mr.transform.Rotate(Vector3.up * Time.deltaTime * 50);
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Player") {
+			mr.enabled = false;
+			mc.enabled = false;
+			source.PlayOneShot (collectedSound);
+			other.gameObject.GetComponent<UndergroundCharacter>().IncreaseHP(HP_AMOUNT);
+		}
+	}
+}
---
+++ Assets/Scripts/SmallHPRotator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallHPRotator : MonoBehaviour {
+
+	private MeshRenderer mr;
+	private MeshCollider mc;
+	private AudioSource source;
+	private float HP_AMOUNT = 25.0f;
+	public AudioClip collectedSound;
+
+
+
+	// Use this for initialization
+	void Start () {
+		mr = GetComponent<MeshRenderer> ();
+		mc = GetComponent<MeshCollider> ();
+		source = GetComponent<AudioSource> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mr.transform.Rotate(Vector3.up * Time.deltaTime * 50);
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Player") {
+			mr.enabled = false;
+			mc.enabled = false;
+			source.PlayOneShot (collectedSound);
+			other.gameObject.GetComponent<UndergroundCharacter>().IncreaseHP(HP_AMOUNT);
+		}
+	}
+}
