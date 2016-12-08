cdb7e1d766bea83947fb58e04420a0717b9a2946
Sangmin Lee <ibombing@gmail.com>
2016-12-08 15:59:28 -0500

    EndingScreenScript added

---
+++ Assets/Scripts/EndingScreenScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingScreenScript : MonoBehaviour {
+	private AudioSource source;
+	public ParticleSystem particle;
+	public AudioClip movingSound;
+
+	void Start () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	void Update () {
+		if (PlayerItemManager.r4_num_keys >= 1) {
+			source.PlayOneShot (movingSound);
+			particle.Stop ();
+			// Ending Screen should be hooked up here
+
+		}
+	}
+}
